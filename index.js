7a7addd2a90fd25b2184540baa19a8bf76fca48a
Claire Sersun <claire.sersun@gmail.com>
2022-07-28 15:24:01 -0400

    first working session

--- index.js
+++ index.js
@@ -9,7 +9,29 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = (poem) => {
+  console.log(poem)
+  //This string should consist of an h2 element containing the title of the poem
+  const title = makeTag('h2')(poem[0].title)
+  //an em element containing "by " and the author's name that is itself inside of an h3element, 
+  const authorsNameEm = makeTag('em')(`by ${poem[0].author}`)
+  const authorsName = makeTag('h3')(authorsNameEm)
+
+  //and then paragraph elements for each stanza of the poem that contain lines separated by linebreak tags.the last line in each paragraph tag does NOT contain a linebreak element after it.
+  // const poemLines = poem[0].lines
+  // const breaksBetween = poemLines.map(" ", )
+  
+  // const poemP = makeTag('p')(poemLines)
+  // const poemBr = makeTag('br')(poemP)
+  
+  const makePoem = `${title} ${authorsName}`
+  // `${poemBr}`
+
+  //HTML String:
+  makePoem
+  console.log(makePoem)
+  
+}
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
